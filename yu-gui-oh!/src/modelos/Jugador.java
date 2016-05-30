cf9245c6bc93ec4cd40df4e6d54c49479beb5958
franco <franco@192.168.1.37>
2016-05-30 01:04:32 -0300

    botones de la batalla(solo algunos)

--- yu-gui-oh!/src/modelos/Jugador.java
+++ yu-gui-oh!/src/modelos/Jugador.java
@@ -14,6 +14,7 @@ public class Jugador {
     //Atributos de la clase Jugador
     private String usuario;
     private String contraseña;
+    private int puntaje;
     
     //Constructores de la clase
     public Jugador(String jug, String cont){
@@ -31,11 +32,17 @@ public class Jugador {
     public String getContraseña(){
         return this.contraseña;
     }
+    public int getPuntaje(){
+        return this.puntaje;
+    }
     public String setUsuario(String usuario){
         return this.usuario = usuario;
     }
     public String setContraseña(String contraseña){
         return this.contraseña = contraseña;
+    }
+    public int setPuntaje(int puntaje){
+        return this.puntaje = puntaje;
     }
     
     public boolean ingresar (String usuario, String contraseña){
